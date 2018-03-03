c30314288078c19fecd35ae6a96ab455e10b936d
tfukumori <t_fukumori@mac.com>
2018-03-03 15:40:48 +0900

    test

---
+++ TDDSample.Web.Tests/RentalTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Xunit;
+using TDDSample.Web.Models.Rentals;
+
+namespace TDDSample.Web.Tests
+{
+    public class RentalTest
+    {
+        private readonly Movie _movieNewRelease;
+        private readonly Movie _movieRegular;
+        private readonly Customer _customer;
+
+        public RentalTest()
+        {
+            _movieNewRelease = new Movie("スターウォーズ", MovieRentalType.NewRelease);
+            _customer = new Customer("太郎");
+            _movieRegular = new Movie("ドラえもん", MovieRentalType.Regular);
+        }
+
+        [Fact]
+        public void 新作を一泊したら300円()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("300 円", result);
+        }
+
+        [Fact]
+        public void 新作を三泊したら900円()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 3));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal("900", result);
+        }
+
+        [Fact]
+        public void 新作を四泊したら1200円カンマが入る()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 4));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("1200 円", result);
+        }
+
+        [Fact]
+        public void 普通を二泊借りたら200円()
+        {
+            _customer.AddRental(new Rental(_movieRegular, 2));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("200 円", result);
+        }
+
+        [Fact]
+        public void 新作一泊普通を一泊借りたら500円パターン1()
+        {
+            _customer.AddRental(new Rental(_movieRegular, 1));
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("500 円", result);
+        }
+
+        [Fact]
+        public void 新作一泊普通を一泊借りたら500円パターン2()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+            _customer.AddRental(new Rental(_movieRegular, 1));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("500 円", result);
+        }
+
+        [Fact]
+        public void 新作一泊普通を二泊借りたら500円パターン1()
+        {
+            _customer.AddRental(new Rental(_movieRegular, 2));
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("500 円", result);
+        }
+
+        [Fact]
+        public void 新作一泊普通を二泊借りたら500円パターン2()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+            _customer.AddRental(new Rental(_movieRegular, 2));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("500 円", result);
+        }
+
+        [Fact]
+        public void 新作一泊普通を三泊借りたら600円()
+        {
+            _customer.AddRental(new Rental(_movieNewRelease, 1));
+            _customer.AddRental(new Rental(_movieRegular, 3));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("700 円", result);
+        }
+    }
+}
--- TDDSample.Web/Models/Rentals/Customer.cs
+++ TDDSample.Web/Models/Rentals/Customer.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TDDSample.Web.Models.Rentals
 {
-    public sealed class Customer{
+    public sealed class Customer
+    {
         private readonly List<Rental> _rentals = new List<Rental>();
 
         public string Name { get; }
@@ -16,13 +18,58 @@ namespace TDDSample.Web.Models.Rentals
         {
             _rentals.Add(rental);
         }
-        
+
         /**
          * レンタル金額を円で返す。
          */
         public string Statement()
         {
-            return null;
+            return $"{RentalFee():N0} 円";
+        }
+
+        public string RentalFee()
+        {
+            var 料金 = 0M;
+
+            foreach (var item in _rentals)
+            {
+                switch (item.Movie.RentalType)
+                {
+                    case MovieRentalType.Children:
+                        break;
+                    case MovieRentalType.Regular:
+                        料金 += GetRentalFee普通(item);
+                        break;
+                    case MovieRentalType.NewRelease:
+                        料金 += GetRentalFee新作(item);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return 料金.ToString();
+        }
+
+        private decimal GetRentalFee新作(Rental item)
+        {
+            var 料金 = item.DaysRented * 300M;
+            return 料金;
+        }
+
+        private decimal GetRentalFee普通(Rental item)
+        {
+            var 料金 = 200M;
+            if (item.DaysRented <= 2)
+            {
+                return 料金;
+            }
+            else
+            {
+                return (item.DaysRented - 2) * 200M;
+            }
+
+            throw new InvalidOperationException();
         }
     }
 }
